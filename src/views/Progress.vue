152dd5526d3bd748c1472f650bc2971fa6e4c88d
Samuel <samuelsital@gmail.com>
2019-09-04 20:15:13 +0200

    Change color

--- src/views/Progress.vue
+++ src/views/Progress.vue
@@ -2,7 +2,6 @@
   <div class="container">
     <div class="container-row2">
       <div class="container-cell cell3">
-
         <h1 class="title">
           €344 <span id="h1-small">of €550 raised this month</span>
         </h1>
@@ -11,20 +10,27 @@
 
         <div class="progress-wrapper">
           <div class="progress">September</div>
-          
-          <h4>11 Supporters &nbsp;&nbsp;&nbsp;&nbsp; 16 Recipients &nbsp;&nbsp;&nbsp;&nbsp; 62% coverage</h4>
+
+          <h4>
+            11 Supporters &nbsp;&nbsp;&nbsp;&nbsp; 16 Recipients
+            &nbsp;&nbsp;&nbsp;&nbsp; 62% coverage
+          </h4>
         </div>
-        
+
         <br />
         <!-- <h3>Matching Energy Demand</h3> -->
-        <p><b>3 more donators are needed </b> in order to match the energy bills for the energy poor housholds in your neighborhood.</p>
+        <p>
+          <b>3 more donators are needed </b> in order to match the energy bills
+          for the energy poor housholds in your neighborhood.
+        </p>
 
         <p>
-          Do you know any like-minded residents in your neighborhood, in posession of solar panels?
-          You can invite them with your <b>referral link</b>:
+          Do you know any like-minded residents in your neighborhood, in
+          posession of solar panels? You can invite them with your
+          <b>referral link</b>:
         </p>
-          
-          <!-- <div style="text-align: center">
+
+        <!-- <div style="text-align: center">
             <a href="">www.enershare.eu/referral?id=8DSfdLdska3</a> &nbsp;<span class="copyButton"> <i class="fa fa-copy"></i> Copy</span>
           </div> -->
         <div class="link-wrapper">
@@ -37,14 +43,20 @@
 
         <div class="progress-wrapper prediction">
           <div class="progress" id="prediction1">November</div>
-          <h4>14 Supporters &nbsp;&nbsp;&nbsp;&nbsp; 19 Recipients &nbsp;&nbsp;&nbsp;&nbsp; 67% coverage</h4>
+          <h4>
+            14 Supporters &nbsp;&nbsp;&nbsp;&nbsp; 19 Recipients
+            &nbsp;&nbsp;&nbsp;&nbsp; 67% coverage
+          </h4>
         </div>
 
         <br />
 
-          <div class="progress-wrapper prediction">
+        <div class="progress-wrapper prediction">
           <div class="progress" id="prediction2">December</div>
-          <h4>16 Supporters &nbsp;&nbsp;&nbsp;&nbsp; 21 Recipients &nbsp;&nbsp;&nbsp;&nbsp; 74% coverage</h4>
+          <h4>
+            16 Supporters &nbsp;&nbsp;&nbsp;&nbsp; 21 Recipients
+            &nbsp;&nbsp;&nbsp;&nbsp; 74% coverage
+          </h4>
         </div>
       </div>
       <div class="container-cell cell4">
@@ -147,7 +159,7 @@ export default {
   width: 0%;
   height: 100%;
 
-  background-color: $primary;
+  background-color: #69d8d4;
   border-radius: 5px;
 
   animation: 1s ease 0s forwards growProgress;
@@ -187,43 +199,55 @@ h4 {
 }
 
 .copyButton {
-	-moz-box-shadow:inset 0px 1px 0px 0px #ffffff;
-	-webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;
-	box-shadow:inset 0px 1px 0px 0px #ffffff;
-	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #f9f9f9), color-stop(1, #e9e9e9));
-	background:-moz-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
-	background:-webkit-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
-	background:-o-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
-	background:-ms-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
-	background:linear-gradient(to bottom, #f9f9f9 5%, #e9e9e9 100%);
-	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#f9f9f9', endColorstr='#e9e9e9',GradientType=0);
-	background-color:#f9f9f9;
-	-moz-border-radius:6px;
-	-webkit-border-radius:6px;
-	border-radius:6px;
-	border:1px solid #dcdcdc;
-	display:inline-block;
-	cursor:pointer;
-	color:#666666;
-	font-size:12px;
-	font-weight:bold;
-	padding:6px 12px;
-	text-decoration:none;
-	text-shadow:0px 1px 0px #ffffff;
+  -moz-box-shadow: inset 0px 1px 0px 0px #ffffff;
+  -webkit-box-shadow: inset 0px 1px 0px 0px #ffffff;
+  box-shadow: inset 0px 1px 0px 0px #ffffff;
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0.05, #f9f9f9),
+    color-stop(1, #e9e9e9)
+  );
+  background: -moz-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+  background: -webkit-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+  background: -o-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+  background: -ms-linear-gradient(top, #f9f9f9 5%, #e9e9e9 100%);
+  background: linear-gradient(to bottom, #f9f9f9 5%, #e9e9e9 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#f9f9f9', endColorstr='#e9e9e9',GradientType=0);
+  background-color: #f9f9f9;
+  -moz-border-radius: 6px;
+  -webkit-border-radius: 6px;
+  border-radius: 6px;
+  border: 1px solid #dcdcdc;
+  display: inline-block;
+  cursor: pointer;
+  color: #666666;
+  font-size: 12px;
+  font-weight: bold;
+  padding: 6px 12px;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #ffffff;
 }
 .copyButton:hover {
-	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #e9e9e9), color-stop(1, #f9f9f9));
-	background:-moz-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
-	background:-webkit-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
-	background:-o-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
-	background:-ms-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
-	background:linear-gradient(to bottom, #e9e9e9 5%, #f9f9f9 100%);
-	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#e9e9e9', endColorstr='#f9f9f9',GradientType=0);
-	background-color:#e9e9e9;
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0.05, #e9e9e9),
+    color-stop(1, #f9f9f9)
+  );
+  background: -moz-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+  background: -webkit-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+  background: -o-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+  background: -ms-linear-gradient(top, #e9e9e9 5%, #f9f9f9 100%);
+  background: linear-gradient(to bottom, #e9e9e9 5%, #f9f9f9 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#e9e9e9', endColorstr='#f9f9f9',GradientType=0);
+  background-color: #e9e9e9;
 }
 .copyButton:active {
-	position:relative;
-	top:1px;
+  position: relative;
+  top: 1px;
 }
 
 @keyframes progress-animation {
